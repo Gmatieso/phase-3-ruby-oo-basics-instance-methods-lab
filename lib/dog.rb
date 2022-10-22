85c2f9deb85e44deef5d9f507d441fa3947757e9
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-22 18:09:52 +0300

    :wrench: instance sit Method Definition

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,24 @@
 class Dog
     # Class body 
 
-    #instance Method Definition
+    #instance bark Method Definition
     def bark 
         puts "Woof!"
     end
+    
+        
+    #instance  sit Method Definition
+    def sit 
+        puts "The Dog is sitting"
+    end
+
+
 end 
 
 #creating instance of our new dog 
 fido = Dog.new
+#let the dog bark 
 fido.bark
+#let the dog sit 
+fido.sit
+
