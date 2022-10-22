c24d3ba036753f4852d250a0323959fb917d365a
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-22 18:18:18 +0300

    :wrench: instance walk Method Definition

--- lib/person.rb
+++ lib/person.rb
@@ -4,8 +4,15 @@ class Person
     def talk 
         puts "Hello World!"
     end
+
+    #instance walk Method Definition
+    def walk 
+        puts "The Person is walking"
+    end
 end
 #Creating instance of our new Person
 geoffrey = Person.new
 #let the person speak
 geoffrey.talk
+#let the person walk
+geoffrey.walk
