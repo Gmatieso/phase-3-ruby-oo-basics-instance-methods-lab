404138cd23b202aef473d14f5b179eefe31df252
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-22 18:14:58 +0300

    :wrench: instance talk Method Definition

---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person
+    #Class body 
+    #instance talk Method Definition
+    def talk 
+        puts "Hello World!"
+    end
+end
+#Creating instance of our new Person
+geoffrey = Person.new
+#let the person speak
+geoffrey.talk
